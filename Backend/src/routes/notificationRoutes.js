84deaeba1702dfa7285c298991949ebf703d0d6d
om-patil-builds <vp8843o3@gmail.com>
2026-06-27 14:57:08 +0530

    Develop Notification Feature

--- Backend/src/routes/notificationRoutes.js
+++ Backend/src/routes/notificationRoutes.js
@@ -6,9 +6,12 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const {
   getNotifications,
   markAsRead,
+  markAllAsRead,
 } = require("../controllers/notificationController");
 
 router.get("/", authMiddleware, getNotifications);
+
+router.put("/read-all", authMiddleware, markAllAsRead);
 
 router.put("/:id/read", authMiddleware, markAsRead);
 
